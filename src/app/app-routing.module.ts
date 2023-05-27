0d35195d9e547d7506a87df8b19cfc64c150453e
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2023-05-27 23:28:14 +0500

    implemented cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'products',
     component: ProductsComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { BaseUrlInterceptor } from './services/http/base-url.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { SkeletonComponent } from './components/shared/skeleton/skeleton.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,7 @@ import { SkeletonComponent } from './components/shared/skeleton/skeleton.compone
     HomeComponent,
     ProductsComponent,
     SkeletonComponent,
+    CartComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
