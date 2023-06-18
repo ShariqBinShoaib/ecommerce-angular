8be77a7bdfd5d61901820747afb806beacbf3577
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2023-06-18 22:03:23 +0500

    fixed code splitting issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,9 @@ import { BaseUrlInterceptor } from './services/http/http.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './modules/shared/shared.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { MainModule } from './modules/main/main.module';
-import { AboutComponent } from './pages/about/about.component';
-
 @NgModule({
-  declarations: [AppComponent, AboutComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    SharedModule,
-    AuthModule,
-    MainModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
